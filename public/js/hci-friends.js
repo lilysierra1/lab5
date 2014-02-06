a8227fc3b593ccd8a22b53c23978a8d2a538668a
vagrant <vagrant@precise32.(none)>
2014-02-06 00:31:18 +0000

    lab 5 stuff

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".friend h3").click(listenerFunction);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,12 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+
+}
+function listenerFunction(e) {
+	e.preventDefault();
+	console.log("you clicked a name");
+	var name = $(this).text();
+	var newName = anagrammedName(name);
+	$(this).text(newName);
 }
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	var name = req.query.name;
+	var des = req.query.description;
+
+	var newFriend={
+		"name": name,
+		"description": des,
+		"imageURL": "http://lorempixel.com/400/400/people"
+	};
+	data["friends"].push(newFriend);
+	console.log("you have a new friend" + newFriend);
+	
+	res.render('add');
  }
